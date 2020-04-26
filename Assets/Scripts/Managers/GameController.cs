d46cfc2be5f25a26ac0652df3a896461d3931384
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-04-26 13:20:53 +0200

    Audio Manager
    Audio Manager

---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance { get; private set; }
+
+    public int lives;
+
+    public int hiscore = 0;
+    public int record;
+    public bool isrecord;
+
+    public float energy;
+
+    public bool GameON;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        if (hiscore > record) { isrecord = true; record = hiscore; }
+        if (GameON == true)
+        {
+            hiscore += 1;
+        }
+    }
+}
+    

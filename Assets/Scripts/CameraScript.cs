84efa55dc2422b5b6b0811641d443cafdf4aaf26
AlexBarra <alexbarra00@gmail.com>
2020-04-26 11:35:10 +0200

    Added More Things
    -Added More Things

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameObject Player;
+    private Vector3 Position = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Position.x = Player.transform.position.x;
+        Position.y = Player.transform.position.y;
+        Position.z = transform.position.z;
+        transform.position = Position;
+    }
+}

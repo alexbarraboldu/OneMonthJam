a29200e703de00a0b1258211a863ed6f32bea62b
AlexBarra <alexbarra00@gmail.com>
2020-04-26 17:52:45 +0200

    More Fixing

--- Assets/Scripts/Entities/Bullet.cs
+++ Assets/Scripts/Entities/Bullet.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public bool lmao;
-    void Start()
-    {
-        Destroy(gameObject, 1.0f);
+    //public bool lmao;
+    //void Start()
+    //{
+    //    Destroy(gameObject, 1.0f);
 
-    }
+    //}
 
-    void OnTriggerEnter2D(Collider2D c)
-    {
-        if (c.gameObject.tag.Equals("Enemy"))
-        {
-            Destroy(c.gameObject);
-            GameController.Instance.IncrementScore();
+    //void OnTriggerEnter2D(Collider2D c)
+    //{
+    //    if (c.gameObject.tag.Equals("Enemy"))
+    //    {
+    //        Destroy(c.gameObject);
+    //        GameController.Instance.IncrementScore();
 
-        }
-        if (c.gameObject.tag.Equals("edges"))
-        {
-            Destroy(gameObject);
-        }
-    }
+    //    }
+    //    if (c.gameObject.tag.Equals("edges"))
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
--- Assets/Scripts/Entities/enemy.cs
+++ Assets/Scripts/Entities/enemy.cs
@@ -47,6 +47,8 @@ public class Enemy : MonoBehaviour
 		{
 			Destroy(collision.gameObject);
 			Destroy(gameObject);
+		    GameController.Instance.IncrementScore();
+
 		}
 	}
 }

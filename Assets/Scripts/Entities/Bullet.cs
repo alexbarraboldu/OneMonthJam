52738ff96f1ea0ffbda181dd5dd322b3d391e80a
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-04-26 16:37:06 +0200

    Bullet Script

--- Assets/Scripts/Entities/Bullet.cs
+++ Assets/Scripts/Entities/Bullet.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public bool lmao;
+    void Start()
+    {
+        Destroy(gameObject, 1.0f);
+        GetComponent<Rigidbody2D>()
+            .AddForce(transform.up * 400);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(c.gameObject);
+            GameController.Instance.IncrementScore();
+
+        }
+        if (c.gameObject.tag.Equals("edges"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }

4eff0a02d57a0d8437e68559617a608eeab2dbe1
AlexBarra <alexbarra00@gmail.com>
2020-04-26 23:01:27 +0200

    Fixed GameController
    Fixed GameController

--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -7,21 +7,44 @@ using System.IO;
 
 public class GameController : MonoBehaviour
 {
-    public static GameController instance { get; private set; }
+    public static GameController Instance { get; private set; }
     
     public int hiscore = 0;
     public int record;
 
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-        if (instance == null)
-            instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
         else
         {
-            Destroy(gameObject);
-            return;
+            Debug.Log("Error: Duplicated " + this + "in the scene");
         }
+    }
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.Log("Error: Duplicated " + this + "in the scene");
+        }
+
+
+        FindObjectOfType<AudioManager>().Play("MenuMusic");
+        StreamReader reader = new StreamReader("Hiscore.txt");
+        string hiscorestring = reader.ReadLine();
+        hiscore = int.Parse(hiscorestring);
+        reader.Close();
+       // BeginGame();
+
     }
 
     // Update is called once per frame
@@ -33,6 +56,25 @@ public class GameController : MonoBehaviour
 
     void FixedUpdate()
     {
+        score = 0;
+        //scoreController.Instance.textScore.text = "PUNTOS:" + score;
+        //scoreController.Instance.textHiscore.text = "META: " + hiscore;
+
     }
-}
+    public void IncrementScore()
+    {
+        score++;
+
+        //scoreController.Instance.textScore.text = "PUNTOS:" + score;
+        if (score > hiscore)
+        {
+            hiscore = Instance.score;
+            //scoreController.Instance.textHiscore.text = "META: " + hiscore;
+
+            // Save the new hiscore
+            StreamWriter writer = new StreamWriter("Hiscore.txt", true);
+            writer.Write(hiscore);
+            writer.Close();
+        }
+    }
     

a57c91eb9f06c67433448cac366718efd0359d73
AlexBarra <alexbarra00@gmail.com>
2020-04-27 21:51:01 +0200

    MORE BUG FIXING

--- Assets/Scripts/Entities/Core.cs
+++ Assets/Scripts/Entities/Core.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 
 public class Core : MonoBehaviour
 {
-    public int life;
-    public Slider lifeslider;
-    private Animator anim;
-    private void Start()
-    {
-        anim = gameObject.GetComponent<Animator>();
-        //if (lifeslider == null) lifeslider = GetComponent<Slider>();
-    }
-    private void Update()
-    {
-       if (lifeslider != null) lifeslider.value = life;
-        if (life <= 0)
-        {
-            Destroy(gameObject);
-            PlayerManager.Instance.DefeatScene();
-        }
-    }
+	public int life;
+	public Slider lifeslider;
+	private Animator anim;
+	private void Start()
+	{
+		anim = gameObject.GetComponent<Animator>();
+		//if (lifeslider == null) lifeslider = GetComponent<Slider>();
+	}
+	private void Update()
+	{
+		if (lifeslider != null) lifeslider.value = life;
+		if (life <= 0)
+		{
+			Destroy(gameObject);
+			if (lifeslider != null) PlayerManager.Instance.DefeatScene();
+		}
+	}
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-           // anim.SetBool("Hit", true);
-            life -= PlayerManager.Instance.enemyDmg;
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Enemy")
+		{
+			anim.SetBool("Hit", true);
+			life -= PlayerManager.Instance.enemyDmg;
 
-        }
-        
-    }
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag.Equals("Enemy"))
-        {
-            anim.SetBool("Hit", false);
+		}
+		
+	}
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag.Equals("Enemy"))
+		{
+			anim.SetBool("Hit", false);
 
-        }
-    }
+		}
+	}
 }

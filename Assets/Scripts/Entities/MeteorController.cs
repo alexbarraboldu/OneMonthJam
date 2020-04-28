0bfc1288a927cce014489d8798fbbc7002d4738c
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-04-28 10:46:12 +0200

    AmmoLoot

---
+++ Assets/Scripts/Entities/MeteorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour
+{
+    public GameObject loot; 
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            GameObject explosionclone = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            GameObject lootclone = Instantiate(loot, this.transform.position, Quaternion.identity);
+            Destroy(explosionclone, 1.0f);
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject explosionclone = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            GameObject lootclone = Instantiate(loot, this.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(explosionclone, 1.0f);
+        }
+    }
+}

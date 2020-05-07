50dc202fd93ce5d506c412b32cd597abfda8cc26
Enekai <gerardpr2001@gmail.com>
2020-05-07 10:17:15 +0200

    xd
    xd

---
+++ Assets/Scripts/Entities/FakeCore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeCore : MonoBehaviour
+{
+    public int life;
+
+    void Update()
+    {
+        if (life <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            life -= PlayerManager.Instance.enemyDmg;
+    }
+
+}
